c48c32f669815823f8f4a94b9e8145266224aba6
J.Coleman <jcoleman@dovico.com>
2017-08-24 13:56:20 -0300

    updates to cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -3,8 +3,39 @@ var ActionPlan = Parse.Object.extend("ActionPlans");
 Parse.Cloud.define("createNewActionPlan", function(request, response) {
 	if(request.user){
 		var plan = new ActionPlan();
+		var columns = [{
+			title: "Budget",
+		},{
+			title: "Indicator",
+		},{
+			title: "Deadline",
+		},{
+			title: "Resources",
+		},{
+			title: "Activities",
+		},{
+			title: "Objectives",
+		}]
+		var rows = [{
+			title: "Axis",
+			columns: [{
+				text: "null"
+			},{
+				text: "null"
+			},{
+				text: "null"
+			},{
+				text: "null"
+			},{
+				text: "null"
+			},{
+				text: "null"
+			}]
+		}]
 	    plan.set("title",request.params.title)
 	    plan.set("description",request.params.description)
+	    plan.set("columns",columns)
+	    plan.set("rows",rows)
 	    plan.set("createdBy",request.user)
 	    plan.save()
 	        .then(function(results) {
