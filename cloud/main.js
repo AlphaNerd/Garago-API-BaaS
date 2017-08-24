bebeea1170441618f3fed3043b172822b7e9f249
J.Coleman <jcoleman@dovico.com>
2017-08-24 15:41:38 -0300

    updates to cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,96 +1,97 @@
 var ActionPlan = Parse.Object.extend("ActionPlans");
 
 Parse.Cloud.define("createNewActionPlan", function(request, response) {
-	if(request.user){
-		var plan = new ActionPlan();
-		var columns = [{
-			title: "Budget",
-			style: {
-				"background":"#f3f3f3",
-				"color":"#333"
-			}
-		},{
-			title: "Indicator",
-			style: {
-				"background":"#f3f3f3",
-				"color":"#333"
-			}
-		},{
-			title: "Deadline",
-			style: {
-				"background":"#f3f3f3",
-				"color":"#333"
-			}
-		},{
-			title: "Resources",
-			style: {
-				"background":"#f3f3f3",
-				"color":"#333"
-			}
-		},{
-			title: "Activities",
-			style: {
-				"background":"#f3f3f3",
-				"color":"#333"
-			}
-		},{
-			title: "Objectives",
-			style: {
-				"background":"#f3f3f3",
-				"color":"#333"
-			}
-		}]
-		var rows = [{
-			title: "Axis",
-			columns: [{
-				text: "null",
-				locked: false
-			},{
-				text: "null",
-				locked: false
-			},{
-				text: "null",
-				locked: false
-			},{
-				text: "null",
-				locked: false
-			},{
-				text: "null",
-				locked: false
-			},{
-				text: "null",
-				locked: false
-			}]
-		}]
-		
-		///// Set User ACL Privelages
-		var acl = new Parse.ACL();
-		acl.setPublicReadAccess(false);
-		acl.setReadAccess(request.user.id, true);
-		acl.setWriteAccess(request.user.id, true);
-		plan.setACL(acl);
+    if (request.user) {
+        var plan = new ActionPlan();
 
-		///// Set object properties for new Action Plan
-	    plan.set("title",request.params.title)
-	    plan.set("description",request.params.description)
-	    plan.set("columns",columns)
-	    plan.set("rows",rows)
-	    plan.set("createdBy",request.user)
-	    plan.set("locked",false)
-	    plan.set("settings",{})
-	    plan.set("footer","")
-	    plan.set("owners",[])
+        ///// Set User ACL Privelages
+        var acl = new Parse.ACL();
+        acl.setPublicReadAccess(false);
+        acl.setReadAccess(request.user.id, true);
+        acl.setWriteAccess(request.user.id, true);
+        plan.setACL(acl);
 
-	    ///// Save to MongoDB
-	    plan.save()
-	        .then(function(results) {
-	            console.log(results)
-	            response.success(results);
-	        })
-	        .catch(function(e) {
-	            response.error(e);
-	        });
-	}else{
-		response.error("User must be logged in to create plan.")
-	}
+        ///// Set object properties for new Action Plan
+        plan.set("title", request.params.title)
+        plan.set("description", request.params.description)
+        plan.set("columns", columns)
+        plan.set("rows", rows)
+        plan.set("createdBy", request.user)
+        plan.set("locked", false)
+        plan.set("settings", {})
+        plan.set("footer", "")
+        plan.set("owners", [])
+
+        ///// Save to MongoDB
+        plan.save()
+            .then(function(results) {
+                console.log(results)
+                response.success(results);
+            })
+            .catch(function(e) {
+                response.error(e);
+            });
+    } else {
+        response.error("User must be logged in to create plan.")
+    }
 });
+
+var columns = [{
+    title: "Budget",
+    style: {
+        "background": "#f3f3f3",
+        "color": "#333"
+    }
+}, {
+    title: "Indicator",
+    style: {
+        "background": "#f3f3f3",
+        "color": "#333"
+    }
+}, {
+    title: "Deadline",
+    style: {
+        "background": "#f3f3f3",
+        "color": "#333"
+    }
+}, {
+    title: "Resources",
+    style: {
+        "background": "#f3f3f3",
+        "color": "#333"
+    }
+}, {
+    title: "Activities",
+    style: {
+        "background": "#f3f3f3",
+        "color": "#333"
+    }
+}, {
+    title: "Objectives",
+    style: {
+        "background": "#f3f3f3",
+        "color": "#333"
+    }
+}]
+var rows = [{
+    title: "Axis",
+    columns: [{
+        text: "null",
+        locked: false
+    }, {
+        text: "null",
+        locked: false
+    }, {
+        text: "null",
+        locked: false
+    }, {
+        text: "null",
+        locked: false
+    }, {
+        text: "null",
+        locked: false
+    }, {
+        text: "null",
+        locked: false
+    }]
+}]
