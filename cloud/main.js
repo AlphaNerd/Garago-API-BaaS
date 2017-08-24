f8c0298a807b5f7ce2734cd10c90370ff845fcaa
J.Coleman <jcoleman@dovico.com>
2017-08-24 13:45:59 -0300

    updates to cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,16 +1,20 @@
 var ActionPlan = Parse.Object.extend("ActionPlans");
 
 Parse.Cloud.define("createNewActionPlan", function(request, response) {
-    var plan = new ActionPlan();
-    plan.set("title",request.params.title)
-    plan.set("description",request.params.description)
-    plan.set("createdBy",request.user)
-    plan.save()
-        .then(function(results) {
-            console.log(results)
-            response.success(results);
-        })
-        .catch(function(e) {
-            response.error(e);
-        });
+	if(request.user){
+		var plan = new ActionPlan();
+	    plan.set("title",request.params.title)
+	    plan.set("description",request.params.description)
+	    plan.set("createdBy",request.user)
+	    plan.save()
+	        .then(function(results) {
+	            console.log(results)
+	            response.success(results);
+	        })
+	        .catch(function(e) {
+	            response.error(e);
+	        });
+	}else{
+		response.error("User must be logged in.")
+	}
 });
