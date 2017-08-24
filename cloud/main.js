042583a3e85ce6a670bc80a357320b55571f07c2
J.Coleman <jcoleman@dovico.com>
2017-08-24 13:36:46 -0300

    updates to cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,16 @@
+var ActionPlan = Parse.Object.extend("ActionPlans");
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define("createNewActionPlan", function(request, response) {
+    var plan = new ActionPlan();
+    plan.set("title",request.params.title)
+    plan.set("description",request.params.description)
+    plan.set("createdBy",request.user)
+    plan.save()
+        .then(function(results) {
+            console.log(results)
+            response.success(results);
+        })
+        .catch(function(e) {
+            response.error(e);
+        });
 });
--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'GaragoAPI',
-  masterKey: process.env.MASTER_KEY || 'B15d1sTh3W05d!', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL,  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["ActionPlans"] // List of classes to support for query subscriptions
   }
@@ -24,7 +24,6 @@ var api = new ParseServer({
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
-
 
 var app = express();
 
@@ -36,20 +35,20 @@ var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get('/test', function(req, res) {
+  res.status(200).send("success");
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/welcome.html'));
 });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server running on port ' + port + '.');
+    console.log('parse-server-example running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
