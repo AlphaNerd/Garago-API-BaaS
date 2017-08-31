31a1d0d404d48c86f2bdd2f3bf6e303d1590bd8f
AlphaNerd <colemanjeff@mac.com>
2017-08-31 18:35:53 -0300

    live query update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL,  // Don't forget to change to https if needed
   allowClientClassCreation: false,
   liveQuery: {
-    classNames: ["ActionPlans","Messages"] // List of classes to support for query subscriptions
+    classNames: ["ActionPlans","Messages","Projects","Organizations"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
