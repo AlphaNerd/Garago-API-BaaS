2e7cc62fad2b7f769e1e68a9ed67c44601e31487
AlphaNerd <colemanjeff@mac.com>
2017-09-06 19:00:32 -0300

    file uploads

--- cloud/main.js
+++ cloud/main.js
@@ -131,7 +131,7 @@ Parse.Cloud.beforeSave("Files", function (request, response) {
     request.object.set("title",title[1])
     var type = request.object.get("file")._name.split(".")
     request.object.set("type",type[type.length-1])
-    request.object.set("createdBy",request.user)
+    request.object.set("createdBy",request.user.id)
     response.success()
 });
 
