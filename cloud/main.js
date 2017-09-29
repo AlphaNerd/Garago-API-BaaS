8ae029d0dcf98809d09b3cc2c3f700cdf2cf7bcd
J.Coleman <jcoleman@dovico.com>
2017-09-29 08:11:35 -0300

    update

--- cloud/main.js
+++ cloud/main.js
@@ -226,8 +226,9 @@ Parse.Cloud.define("addUserFavFile", function (request, response) {
 });
 
 Parse.Cloud.beforeSave("Files", function (request, response) {
-    var title = request.object.get("file")._name.split("_")
-    request.object.set("title",title[1])
+    var title = request.object.get("file").replace("_","#@#")
+    var newTitle = title._name.split("#@#")
+    request.object.set("title",newTitle[1].toLowerCase())
     var type = request.object.get("file")._name.split(".")
     request.object.set("type",type[type.length-1])
 
