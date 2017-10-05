abf8aecdfa3f3c820011a44d346d676bf7207f0c
J.Coleman <jcoleman@dovico.com>
2017-10-05 14:23:09 -0300

    noc codes

--- cloud/main.js
+++ cloud/main.js
@@ -374,6 +374,7 @@ Parse.Cloud.define("getAllUsers", function (request, response) {
 ///////////////////////////////////////////////////////
 Parse.Cloud.define("getNocCodes", function (request, response) {
     var query1 = new Parse.Query(NocCodes)
+    var searchTerm = request.params.searchTerm
     query1.equalTo("title", searchTerm)
 
     var query2 = new Parse.Query(NocCodes)
